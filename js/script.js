cee080f018416a0e171687b6b694c5ab9bfac212
Zaid Hasan <xenon0ae@gmail.com>
2026-02-02 14:48:45 +0530

    wrote some basic js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+
+var docoolstuff = function() { 
+    var currentclass = document.getElementById('cool').className;
+
+    if (currentclass == 'cool') {
+    document.getElementById('cool').className = 'coolorange';
+} else {
+    document.getElementById('cool').className = 'cool';
+}
+};
+
 console.log('Hello, Rocket!');
